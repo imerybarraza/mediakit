e2fa51b873877ebfac3947d6fcfb0cb4f42a6cbe
ivemery <ivette.mery@gmail.com>
2024-07-21 16:47:21 -0400

    changes gt

--- database/migrations/2024_07_20_040706_create_social_count_table.php
+++ database/migrations/2024_07_20_040706_create_social_count_table.php
@@ -9,12 +9,12 @@ class CreateSocialCountTable extends Migration
     public function up()
     {
         Schema::create('social_count', function (Blueprint $table) {
-            $table->id(); // Id de la tabla, auto-incremental
-            $table->unsignedBigInteger('user_id'); // Clave foránea que referencia la tabla users
-            $table->string('platform'); // Nombre de la plataforma (Facebook, Instagram, etc.)
-            $table->string('social_id'); // ID social del usuario en la plataforma
-            $table->string('access_token'); // Token de acceso
-            $table->timestamps(); // Campos created_at y updated_at
+            $table->id(); 
+            $table->unsignedBigInteger('user_id'); 
+            $table->string('platform'); 
+            $table->string('social_id'); 
+            $table->string('access_token'); 
+            $table->timestamps(); 
 
             // Definir clave foránea
             $table->foreign('user_id')->references('iduser')->on('users')->onDelete('cascade');
